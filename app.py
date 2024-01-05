c164db8804a6c48835c612b0a9434f1c60cc2d40
jimi-117 <sho.jimi117@gmail.com>
2024-01-05 16:49:43 +0100

    2nd version

--- app.py
+++ app.py
@@ -1,92 +1,98 @@
-# Prepare environment
 import dash
+import pandas as pd
 from dash import dcc, html
 from dash.dependencies import Input, Output
-from dash.exceptions import PreventUpdate
-import numpy as np
 import plotly.express as px
-import pandas as pd
-import folium
 
-# Load csv
-data = pd.read_csv("first_dash/sources/arbres10percent.csv")
-
-# Data cleaning
-def df_cleaning(dataframe):
-    cols =[
-    # 'IDBASE',
-    # 'TYPE EMPLACEMENT',
-    'DOMANIALITE',
-    # 'ARRONDISSEMENT',
-    # 'COMPLEMENT ADRESSE',
-    # 'NUMERO',
-    # 'LIEU / ADRESSE',
-    # 'IDEMPLACEMENT',
-    # 'LIBELLE FRANCAIS',
-    # 'GENRE',
-    # 'ESPECE',
-    # 'VARIETE OUCULTIVAR',
-    'CIRCONFERENCE (cm)',
-    'HAUTEUR (m)',
-    'STADE DE DEVELOPPEMENT',
-    # 'REMARQUABLE',
-    'geo_point_2d'
+# Load and clean the data
+def load_and_clean_data(filepath):
+    cols_to_use = [
+        'DOMANIALITE', 'CIRCONFERENCE (cm)', 'HAUTEUR (m)',
+        'STADE DE DEVELOPPEMENT', 'geo_point_2d'
     ]
-    df = dataframe[cols].dropna(subset='STADE DE DEVELOPPEMENT')
-    return df
+    df = pd.read_csv(filepath, usecols=cols_to_use).dropna(subset=['STADE DE DEVELOPPEMENT'])
+    
+    coords = df['geo_point_2d'].str.split(',', expand=True)
+    coords.columns = ['lat', 'lon']
+    df[['lat', 'lon']] = coords.astype(float)
+    
+    return df.drop(columns='geo_point_2d')
 
-def df_final(dataframe):
-    temp_df = df_cleaning(dataframe)
-    coords_df = temp_df['geo_point_2d'].str.split(',', expand=True)
-    coords_df.columns = ['lat', 'lon']
-    temp_df[['lat', 'lon']] = coords_df
-    df = temp_df.drop(columns="geo_point_2d")
-    return df
+df = load_and_clean_data("first_dash/sources/arbres10percent.csv")
 
-df = df_final(data)
+# Initialize the Dash app
 
-
-# dash
-
-# init
 app = dash.Dash(__name__)
 
-# application layout
-app.layout = (
-    html.Div(children=[
-        html.H1(children='Degemer Mat !'),
-        dcc.Dropdown(
-            id='domanialite-dropdown', 
-            options=[{'label': i, 'value': i} for i in df['DOMANIALITE'].unique()],
-            value='Alignement'
-            ),
-        dcc.Dropdown(
-            id='stade-development-dropdown', 
-            options=[{'label': i, 'value': i} for i in df['STADE DE DEVELOPPEMENT'].unique()],
-            value='Adulte'
-            ),
-        dcc.Graph(id='graph'),
-        dcc.Graph(id='graph2'),
-        
-    ])
-)
+# Define the layout of the app
+app.layout = html.Div(style={'textAlign': 'center'},children=[
+    html.H1(children='Degemer Mat !', style={'textAlign': 'center'}),
+    
+    dcc.Dropdown(
+        id='domanialite-dropdown', 
+        options=[{'label': i, 'value': i} for i in df['DOMANIALITE'].unique()],
+        value='Alignement',
+        style={
+            'width': '50%',
+            'display' : 'inline-block',
+        }
+    ),
+    dcc.Dropdown(
+        id='stade-development-dropdown', 
+        options=[{'label': i, 'value': i} for i in df['STADE DE DEVELOPPEMENT'].unique()],
+        value='Adulte',
+        style={
+            'width': '50%',
+            'display' : 'inline-block'
+        }
+    ),
+    html.Div(
+        style={'width':'45%', 'display':'inline-block'},
+        children=[dcc.Graph(id='height-histogram')]
+    ),
+    html.Div(
+        style={'width':'45%', 'display':'inline-block'},
+        children=[dcc.Graph(id='circumference-histogram')]
+    ),
+    html.Div(
+        style={'width':'55%','display':'inline-block'},
+        children=[dcc.Graph(id='paris-map')]
+    )    
+])
 
-# callback decolator
 @app.callback(
-    Output('graph', 'figure'),
-    Output('graph2', 'figure'),
+    [Output('height-histogram', 'figure'),
+     Output('circumference-histogram', 'figure'),
+     Output('paris-map', 'figure')],
     [Input('domanialite-dropdown', 'value'), 
      Input('stade-development-dropdown', 'value')]
-           )
-def update_graph(domanialite_value, stade_development_value):
-    filtered_df = df[(df['DOMANIALITE'] == domanialite_value) &
-                     (df['STADE DE DEVELOPPEMENT'] == stade_development_value)]
+)
+def update_graphs(domanialite_value, stade_development_value):
+    if not domanialite_value or not stade_development_value:
+        raise PreventUpdate
     
-    fig = px.histogram(filtered_df, x='HAUTEUR (m)', title='Disp')
-    fig2 = px.histogram(filtered_df, x='CIRCONFERENCE (cm)', title='Disp')
-    return fig, fig2
+    filtered_df = df[(df['DOMANIALITE'] == domanialite_value) & (df['STADE DE DEVELOPPEMENT'] == stade_development_value)]
     
+    height_histogram = px.histogram(filtered_df, x='HAUTEUR (m)', title='Height Distribution')
+    circumference_histogram = px.histogram(filtered_df, x='CIRCONFERENCE (cm)', title='Circumference Distribution')
 
-    
+    paris_map = px.scatter_mapbox(
+        filtered_df,
+        lat='lat',
+        lon='lon',
+        hover_name='DOMANIALITE',
+        hover_data=['STADE DE DEVELOPPEMENT'],
+        size_max=15,
+        opacity=0.6,
+        center={'lat': 48.8566, 'lon': 2.3522},
+        zoom=11,
+        height=600
+    )
+
+    paris_map.update_layout(mapbox_style='open-street-map')
+    paris_map.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+    return height_histogram, circumference_histogram, paris_map
+
 if __name__ == '__main__':
     app.run_server(debug=True)
